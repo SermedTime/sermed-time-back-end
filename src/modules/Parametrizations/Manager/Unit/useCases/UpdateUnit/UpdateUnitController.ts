4a7a2424f51de9bb4bc316eca564d56cb9995ec9
Tainã Pino <tmartins.pino@gmail.com>
2024-04-10 02:40:11 -0300

    feat: add endpoint Update Unit

---
+++ src/modules/Parametrizations/Manager/Unit/useCases/UpdateUnit/UpdateUnitController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { userAuthenticated } from 'services/UserAuthenticated/UserAuthenticated'
+import { UpdateUnitUseCase } from './UpdateUnitUseCase'
+
+class UpdateUnitController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { uuid } = req.params
+
+    const userId = userAuthenticated(req)
+
+    const { unitName, status } = req.body
+
+    const service = container.resolve(UpdateUnitUseCase)
+
+    const unit = await service.execute({
+      uuid,
+      unitName,
+      status,
+      user_action: userId
+    })
+
+    return res.status(unit.status).json(unit)
+  }
+}
+
+export { UpdateUnitController }
--- src/shared/infra/http/routes/Parametrizations/Manager/unit.routes.ts
+++ src/shared/infra/http/routes/Parametrizations/Manager/unit.routes.ts
@@ -4,14 +4,17 @@ import { container } from 'tsyringe'
 import { CreateUnitController } from '@modules/Parametrizations/Manager/Unit/useCases/CreateUnit/CreateUnitController'
 import { DetailsUnitController } from '@modules/Parametrizations/Manager/Unit/useCases/DetailsUnit/DetailsUnitController'
 import { ListUnitController } from '@modules/Parametrizations/Manager/Unit/useCases/ListUnit/ListUnitController'
+import { UpdateUnitController } from '@modules/Parametrizations/Manager/Unit/useCases/UpdateUnit/UpdateUnitController'
 
 const unitRoutes = Router()
 
 const createUnitController = container.resolve(CreateUnitController)
+const updateUnitController = container.resolve(UpdateUnitController)
 const listUnitController = container.resolve(ListUnitController)
 const detailsUnitController = container.resolve(DetailsUnitController)
 
 unitRoutes.post('/', createUnitController.handle)
+unitRoutes.put('/:uuid', updateUnitController.handle)
 unitRoutes.get('/', listUnitController.handle)
 unitRoutes.get('/:uuid', detailsUnitController.handle)
 
