0afad903bd98e719183b851014e0c09f68cb0bb7
Tainã Pino <tmartins.pino@gmail.com>
2024-06-18 22:56:33 -0300

    feat: add validation for user timesheet access permissions

--- src/shared/infra/http/routes/TimeSheet/time_sheet.routes.ts
+++ src/shared/infra/http/routes/TimeSheet/time_sheet.routes.ts
@@ -1,11 +1,16 @@
 import { ListTimeSheetController } from '@modules/TimeSheet/useCases/ListTimeSheet/ListTimeSheetController'
 import { Router } from 'express'
 import { container } from 'tsyringe'
+import { validateUserTimeSheetPermission } from '../../middlewares/validateUserTimeSheetPermission'
 
 const timeSheetRoutes = Router()
 
 const listTimeSheetController = container.resolve(ListTimeSheetController)
 
-timeSheetRoutes.get('/list/:user_id', listTimeSheetController.handle)
+timeSheetRoutes.get(
+  '/list/:user_id',
+  validateUserTimeSheetPermission,
+  listTimeSheetController.handle
+)
 
 export { timeSheetRoutes }
