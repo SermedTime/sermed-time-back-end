b2486ce8f1a17e31fa754af85c6aecd926a2910e
Tainã Pino <tmartins.pino@gmail.com>
2024-06-28 15:27:57 -0300

    refactor: implements app error

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,10 +1,11 @@
-export class AppError {
-  public readonly message: string
-
+export class AppError extends Error {
   public readonly statusCode: number
 
-  constructor(message: string, statusCode = 400) {
-    this.message = message
-    this.statusCode = statusCode
+  constructor(message: string, statusCode: number) {
+    super(message)
+    this.name = 'AppError'
+    this.statusCode = statusCode || 500
+
+    Error.captureStackTrace(this, this.constructor)
   }
 }
